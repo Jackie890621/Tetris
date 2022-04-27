a2b2c10872b0c3f3edf8fea183ec231a79ecbd7c
Li Qian Xi <jackie890621@gmail.com>
2022-04-27 18:01:26 +0800

    Add files via upload
    可以顯示方塊了

--- TetrisController.java
+++ TetrisController.java
@@ -1,12 +1,15 @@
+package com.tetris;
+
+import com.tetris.Shape.Tetrominoe;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.*;
+import javafx.scene.shape.*;
 import java.util.Timer;
 import java.util.TimerTask;
 import javafx.application.Platform;
-
 
 public class TetrisController {
 	@FXML Label top;
@@ -22,35 +25,17 @@ public class TetrisController {
 	@FXML Button multi;
 	@FXML Button start;
 	@FXML Button back;
+	private Timer timer;
+	private Timer gamecycle;
 	
-	@FXML
-	private void singleclicked(ActionEvent e) {
-		single.setVisible(false);
-		multi.setVisible(false);
-				
-		top.setVisible(true);
-		score.setVisible(true);
-		back.setVisible(true);
-		start.setVisible(true);
-		main.setStyle("-fx-background-color: " + "GAINSBORO");
-		next1.setStyle("-fx-background-color: " + "GAINSBORO");
-		next2.setStyle("-fx-background-color: " + "GAINSBORO");
-		next3.setStyle("-fx-background-color: " + "GAINSBORO");
-		next4.setStyle("-fx-background-color: " + "GAINSBORO");
-		next5.setStyle("-fx-background-color: " + "GAINSBORO");
-		hold.setStyle("-fx-background-color: " + "GAINSBORO");
-	}
-	
-	@FXML
-	private void startclicked(ActionEvent e) {
-		start.setDisable(true);
-		countdown();
-	}
-	
-	@FXML
-	private void backclicked(ActionEvent e) {
-		initialize();
-	}
+	private final int BOARD_WIDTH = 10;
+    private final int BOARD_HEIGHT = 20;
+	private final int PERIOD_INTERVAL = 200;
+	private Shape curPiece;
+    private Tetrominoe[] board;
+	private boolean isFallingFinished = false;
+	private int curX = 0;
+    private int curY = 0;
 	
 	public void initialize() {
 		single.setVisible(true);
@@ -72,8 +57,45 @@ public class TetrisController {
 		hold.setStyle("");
 	}
 	
+	@FXML
+	private void singleclicked(ActionEvent e) {
+		single.setVisible(false);
+		multi.setVisible(false);
+				
+		top.setVisible(true);
+		score.setVisible(true);
+		back.setVisible(true);
+		start.setVisible(true);
+		main.setStyle("-fx-background-color: " + "GAINSBORO");
+		next1.setStyle("-fx-background-color: " + "GAINSBORO");
+		next2.setStyle("-fx-background-color: " + "GAINSBORO");
+		next3.setStyle("-fx-background-color: " + "GAINSBORO");
+		next4.setStyle("-fx-background-color: " + "GAINSBORO");
+		next5.setStyle("-fx-background-color: " + "GAINSBORO");
+		hold.setStyle("-fx-background-color: " + "GAINSBORO");
+		
+	}
+	
+	@FXML
+	private void backclicked(ActionEvent e) {
+		timer.cancel();
+		timer.purge();
+		gamecycle.cancel();
+		gamecycle.purge();
+		clearBoard();
+		main.getChildren().clear();
+		initialize();
+	}
+	
+	@FXML
+	private void startclicked(ActionEvent e) {
+		start.setDisable(true);
+		countdown();
+		start();
+	}
+	
 	private void countdown() {
-		Timer timer = new Timer();
+		timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
             int i = 120;
 			String time;
@@ -93,4 +115,153 @@ public class TetrisController {
             }
         }, 0, 1000);
 	}
+	
+	private void start() {
+        curPiece = new Shape();
+        board = new Tetrominoe[BOARD_WIDTH * BOARD_HEIGHT];
+
+        clearBoard();
+        newPiece();
+		gamecycle = new Timer();
+		gamecycle.schedule(new Gamecycle(), 0, PERIOD_INTERVAL);
+	}
+	
+	private void clearBoard() {
+        for (int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH; i++) {
+            board[i] = Tetrominoe.NoShape;
+        }
+    }
+	
+	private void newPiece() {
+        curPiece.setRandomShape();
+        curX = BOARD_WIDTH / 2 + 1;
+        curY = BOARD_HEIGHT - 1 + curPiece.minY();
+	}
+	
+	private class Gamecycle extends TimerTask {   
+		public void run() {   
+			//System.out.println("________");
+			doGameCycle();
+		}	   
+	}
+	
+	private void doGameCycle() {
+        update();
+        drawing();
+    }
+	
+	private void update() {
+		if (isFallingFinished) {
+            isFallingFinished = false;
+            newPiece();
+        } else {
+            oneLineDown();
+        }
+    }
+	
+	private void oneLineDown() {
+        if (!tryMove(curPiece, curX, curY - 1)) {
+            pieceDropped();
+        }
+    }
+	
+	private boolean tryMove(Shape newPiece, int newX, int newY) {
+        for (int i = 0; i < 4; i++) {
+            int x = newX + newPiece.x(i);
+            int y = newY - newPiece.y(i);
+
+            if (x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT) {
+                return false;
+            }
+
+            if (shapeAt(x, y) != Tetrominoe.NoShape) {
+                return false;
+            }
+        }
+
+        curPiece = newPiece;
+        curX = newX;
+        curY = newY;
+
+        drawing();
+
+        return true;
+    }
+	
+	private Tetrominoe shapeAt(int x, int y) {
+        return board[(y * BOARD_WIDTH) + x];
+    }
+	
+	private void pieceDropped() {
+        for (int i = 0; i < 4; i++) {
+            int x = curX + curPiece.x(i);
+            int y = curY - curPiece.y(i);
+            board[(y * BOARD_WIDTH) + x] = curPiece.getShape();
+        }
+
+        //removeFullLines();
+
+        if (!isFallingFinished) {
+            newPiece();
+        }
+    }
+	
+	private void drawing() {
+		Platform.runLater(() -> main.getChildren().clear());
+        int boardTop = 600 - BOARD_HEIGHT * squareHeight();
+
+        for (int i = 0; i < BOARD_HEIGHT; i++) {
+            for (int j = 0; j < BOARD_WIDTH; j++) {
+                Tetrominoe shape = shapeAt(j, BOARD_HEIGHT - i - 1);
+
+                if (shape != Tetrominoe.NoShape) {
+                    drawSquare(j * squareWidth(), boardTop + i * squareHeight(), shape);
+                }
+            }
+        }
+
+        if (curPiece.getShape() != Tetrominoe.NoShape) {
+
+            for (int i = 0; i < 4; i++) {
+                int x = curX + curPiece.x(i);
+                int y = curY - curPiece.y(i);
+
+                drawSquare(x * squareWidth(), boardTop + (BOARD_HEIGHT - y - 1) * squareHeight(), curPiece.getShape());
+            }
+        }
+	}
+	
+	private int squareWidth() {
+        return 300 / BOARD_WIDTH;
+    }
+
+    private int squareHeight() {
+        return 600 / BOARD_HEIGHT;
+    }
+	
+	private void drawSquare(int x, int y, Tetrominoe shape) {
+
+        Color colors[] = {Color.rgb(0, 0, 0), Color.rgb(255, 0, 0),
+						  Color.rgb(0, 255, 0), Color.rgb(0, 255, 255),
+						  Color.rgb(255, 0, 255), Color.rgb(255, 255, 0),
+						  Color.rgb(255, 128, 0), Color.rgb(0, 0, 255)
+        };
+		//for (int i = 0; i < 8; i++) {
+			//System.out.println(colors[i].getRed() + " " + colors[i].getGreen() + " " + colors[i].getBlue());
+		//}
+		
+        //var color = colors[shape.ordinal()];
+		//System.out.println();
+		
+		Rectangle in = new Rectangle();
+		in.setX(x + 1);
+		in.setY(y + 1);
+		in.setWidth(squareWidth() - 2);
+		in.setHeight(squareHeight() - 2);
+		in.setFill(colors[shape.ordinal()]);
+		
+		Platform.runLater(() -> main.getChildren().addAll(in));
+    }
 }
+
+  
